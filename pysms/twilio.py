59df77d784a4ff0758756104ed63a27943a5be55
sirwilliam15 <sirwilliam15@github.com>
2021-04-21 19:46:30 -0700

    Added Twilio Skeleton code

--- pysms/twilio.py
+++ pysms/twilio.py
@@ -1,4 +1,31 @@
+import requests
+import twilio
+from base64 import b64encode
+from datetime import datetime
+from twilio.base.exceptions import TwilioRestException
 
+from .pysms import ApiRequestError, DeviceManager
 
-class Twilio():
-    pass
+class Twilio(DeviceManager):
+    
+    def __init__(self, api_key, account_id):
+        super().__init__()
+        self.client = twilio.rest.Client(account_id, api_key)
+
+    def send_sms(self, message, phone=None, wait=False, timeout=10):
+        """
+        """
+        if phone is None:
+            phone = self.current
+        try:
+            message = self.client.messages.create(
+                to=phone,
+                body=message
+            )
+        except TwilioRestException as e:
+            raise ApiRequestError(e)
+
+    def get_sms_history(self, iccid=None, from_date=datetime.now().date(), all_msgs=False):
+        """
+        """
+        pass
